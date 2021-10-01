92765cf5ab9a3c2645235b8e2d9bbc16253a5f91
Rakib32000 <rh938664@gmail.com>
2021-09-30 20:59:20 -0700

    code commit

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h1>my name is rakib hasan</h1>
+        </div>
+    );
+};
+
+export default Inventory;
